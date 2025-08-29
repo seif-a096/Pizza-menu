07ce8c1c8c6335c67d9b22675aa5672a49667e19
seifatef096 <seifatef2005@gmail.com>
2025-08-29 20:44:53 +0300

    updatng timers logic, sold out data and finishing the project

--- src/App.js
+++ src/App.js
@@ -52,14 +52,16 @@ function App() {
     </div>
   );
 }
-function Pizza(probs) {
+function Pizza(props) {
   return (
-    <li className="pizza">
-      <img src={probs.pizzaObj.photoName} alt={probs.pizzaObj.name} />
+    <li className={`pizza ${props.pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{probs.pizzaObj.name}</h3>
-        <p>{probs.pizzaObj.ingredients}</p>
-        <span>{probs.pizzaObj.price}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>
+          {props.pizzaObj.soldOut ? "Sold out" : props.pizzaObj.price}
+        </span>
       </div>
     </li>
   );
@@ -68,11 +70,17 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} />
-        ))}
-      </ul>
+      <>
+        <p>
+          Authentic Italian cuisine. 6 creative dishes to choose from. All from
+          our stone oven, all organic, all delicious.
+        </p>
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} />
+          ))}
+        </ul>
+      </>
     </main>
   );
 }
@@ -100,13 +108,16 @@ function Footer() {
       {isOpen ? (
         <div className="order">
           <p>
-            - We're currently open till {`${closeH}:00 `}! Come visit us or
-            Order Online!
+            it's {Time} - We're currently open till {`${closeH}:00 `}! Come
+            visit us or Order Online!
           </p>
           <button className="btn">Order</button>
         </div>
       ) : (
-        <p>We're closed!</p>
+        <p>
+          it's {Time} - We're closed! We are happy to welcome you between{" "}
+          {`${openH}:00 and ${closeH}:00`}
+        </p>
       )}
     </footer>
   );
